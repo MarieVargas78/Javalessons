e91ee027f90a7d566aa4b01a4a2172e5ba6768e3
Amber Vargas <AmbermVargas29@gmail.com>
2019-01-19 14:54:00 -0800

    uploading Files

---
+++ ArrayPractice.java
@@ -0,0 +1,55 @@
+public class ArrayPractice {
+     public static void main(String[] arg) {
+     
+        CalculateArea(2, 3); 
+
+    }
+     public static void CalculateArea(int b, int c) {
+         int area = b *c;
+         System.out.println("The area of the rectangle is " + area);
+     }
+    }
+
+
+        
+         
+
+     
+
+
+
+
+
+
+     //Area(7); 
+    
+        
+    //}
+     //public static void Area(int a) {
+        //System.out.println(a*a);
+    
+    
+      
+    //}
+
+
+        
+
+    
+
+        
+        //int[] array = new int [10];
+        //for (int i = 0; i < 10; i++) {
+        // array[i] = i *2 + 2       ;
+        //}
+        //for(int i = 0; i < 10; i++) {
+           // System.out.println(array[i]);
+        //} 
+        //int[] array = new int [10];
+        //for (int i = 0; i < 10; i++) {
+           // array[i] = i *10 + 10;
+
+       // }
+        //for(int i = 0; i < 10; i++) {
+            //System.out.println(array[i]);
+    //}
---
+++ Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] arg) {
+        Boolean isPressed;
+        isPressed = false;
+        String myName = "leo";
+        double variable = 3.14;
+        int count = 0;
+        int[] array = new int[5];
+        array[0] = 5;
+        array[1] = 9;
+        array[2] = 7;
+        String[] otherarray = new String[6];
+        otherarray[0] = "saul";
+        otherarray[1] = "irma";
+        otherarray[2] = "amber";
+        otherarray[3] = "leo";
+        otherarray[4] = "jose";
+        otherarray[5] = "manny";
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("LOOP");
+        }
+    }
+}
